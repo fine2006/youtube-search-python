4c4f88c8816d11f6993194ae1a2024cd3b3b5c98
fine2006 <animeshpathak.2006@gmail.com>
2021-07-14 16:04:21 +0530

    Improve search function

--- ytmdl.py
+++ ytmdl.py
@@ -1,11 +1,32 @@
 def GrabMeta(x):
-    print(search[x] ['title'])
-    print(search[x] ['duration'])
-    val = (search[x] ['artists'])
-    print(val[0] ['name'])
-    vid = (search[x] ['videoId'])
-    print('https://music.youtube.com/watch?v='+vid)
-    print('\n')
+    if (search[x] ['resultType']) == 'playlist':
+        print(search[x] ['title'])
+        print(search[x] ['itemCount'])
+        val = (search[x] ['author'])
+        print(val)
+        browse = (search[x] ['browseId'])
+        print('https://music.youtube.com/playlist?list='+browse)
+        print('\n')
+    elif (search[x] ['resultType']) == 'song':
+        print(search[x] ['title'])
+        print(search[x] ['duration'])
+        val = (search[x] ['artists'])
+        print(val[0] ['name'])
+        vid = (search[x] ['videoId'])
+        print('https://music.youtube.com/watch?v='+vid)
+        print('\n')
+    elif (search[x] ['resultType']) == 'video':
+        print(search[x] ['title'])
+        print(search[x] ['duration'])
+        val = (search[x] ['artists'])
+        print(val[0] ['name'])
+        vid = (search[x] ['videoId'])
+        print('https://music.youtube.com/watch?v='+vid)
+        print('\n')
+    elif (search[x] ['resultType']) == 'artist':
+        pass
+    else:
+        pass
 
 from ytmusicapi import YTMusic
 import json
@@ -15,7 +36,8 @@ ytmusic = YTMusic()
 search = ytmusic.search(search_term)
 with open("data_file.json", "w") as write_file:
     json.dump(search, write_file)
-GrabMeta(0)
-GrabMeta(1)
-GrabMeta(2)
-GrabMeta(3)
+for i in range(0,32767):
+    try:
+        GrabMeta(i)
+    except IndexError:
+        break
